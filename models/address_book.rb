cc2cd9f7f1bc45873c30ee7211f7a3b29c3f20a4
Tim <Jones.tim06@gmail.com>
2016-05-24 12:44:55 -0600

    assignment 21 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry"
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -10,16 +11,33 @@ class AddressBook
 
 
   def add_entry(name, phone_number, email)
-    # #9
     index = 0
     entries.each do |entry|
-      # #10
       if name < entry.name
         break
       end
       index += 1
     end
-    # #11
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
+
+  def import_from_csv_2(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 end
