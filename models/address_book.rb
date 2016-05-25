70e60fa0d0befc0e5a442df6c4229b3472404b18
Tim <Jones.tim06@gmail.com>
2016-05-25 11:42:27 -0600

    checkpoint-22-assigment completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -41,23 +41,13 @@ class AddressBook
     end
   end
 
-  def binary_search(name)
-     lower = 0
-     upper = entries.length - 1
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
 
-     while lower <= upper
-       mid = (lower + upper) / 2
-       mid_name = entries[mid].name
-
-       if name == mid_name
-         return entries[mid]
-       elsif name < mid_name
-         upper = mid - 1
-       elsif name > mid_name
-         lower = mid + 1
-       end
-     end
- 
-     return nil
-   end
- end
+    return nil
+  end
+end
